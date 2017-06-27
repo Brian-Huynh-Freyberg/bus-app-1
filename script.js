c7b2eeddc697606db8a8d9efe63aeaee3c6f8823
BrianHuynh <brianhuynh@students.freyberg.ac.nz>
2017-06-28 11:03:59 +1200

    DRY up code to display correct stops for chosen route and extended function to all available routes

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
+//Grab DOM elements and store into variables
 var routeSelection = document.getElementById("routes");
 //console.log(routeSelection)
 var userRoute;
 var fromLocation = document.getElementById("from-location");
 
+//Get the user selected route - Run everytime the selection is changed
 function getRoute () {
     userRoute = routeSelection.options[routeSelection.selectedIndex].text;
+    userRoute = userRoute.toLowerCase();
+    userRoute += "Stops";
     console.log(userRoute);
 
-    switch (userRoute) {
-        case "1. Awapuni":
-            fromLocation.innerHTML = awapuni();
-            break;
-        case "2. Rugby":
-            fromLocation.innerHTML = rugby();
-            break;
+    //Inserting the stops to the departure selection
+    fromLocation.innerHTML = getStops();
+
+    function getStops () {
+        var content = "<option value = '' disabled selected>";
+        for (var i = 0; i < eval(userRoute).length; i++) {
+            content += "<option>" + eval(userRoute)[i] + "</option>"
+        };
+        content += "</option>"
+        return content
     }
-}
-
-function awapuni() {
-    var content = "<option value = '' disabled selected>";
-    for (var i = 0; i < awapuniStops.length; i++) {
-        content += "<option>" + awapuniStops[i] + "</option>"
-    };
-    content += "</option>"
-    return content
-}
-
-function rugby() {
-    var content = "<option value = '' disabled selected>";
-    for (var i = 0; i < rugbyStops.length; i++) {
-        content += "<option>" + rugbyStops[i] + "</option>"
-    };
-    content += "</option>"
-    return content
 }
 
 
