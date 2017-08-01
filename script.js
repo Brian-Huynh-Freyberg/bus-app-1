d8265ea82bc6755cb93dc26436b0a81e3ef72cd0
Brian-Huynh-Freyberg <BrianHuynh@students.freyberg.ac.nz>
2017-08-01 17:00:11 +1200

    Debug countdown, so there will be no more negative times. DRY up code for selecting the correct timetable of day AND bus app can now deal with the "Home Only" times. Insert Google Maps on index.html

--- script.js
+++ script.js
@@ -60,6 +60,18 @@ var userArrival;
 var userArrivalIndex;
 var remainingStops;
 
+//Google Maps
+var theSquare = {
+        lat: -40.353085,
+        lng: 175.610677
+    }
+
+    //Create a new Map object
+    window.map = new google.maps.Map(document.getElementById('map'), {
+        center: theSquare,
+        zoom: 13
+    })
+
 //Get the user selected route - Run everytime the selection is changed
 function getRoute () {
     userRoute = routeSelection.options[routeSelection.selectedIndex].text
@@ -123,89 +135,135 @@ function arrivalTime() {
 function getTimes() {
         var timeNow = new Date().getHours() + "." + ("0" + new Date().getMinutes()).slice(-2);
         console.log (timeNow);
-        for (var i = 0; i < routes[userRoute].timesMonFri.length; i++) {
-            if (timeNow < routes[userRoute].timesMonFri[i][userDepartureIndex]) {
-                var nextDepartingTime = routes[userRoute].timesMonFri[i][userDepartureIndex];
+        var dayOfWeek = new Date().getDay();
+        console.log(dayOfWeek);
+
+        //Getting the the correct timetable and first time for the selected stop for tomorrow
+        if (dayOfWeek > -1 && dayOfWeek < 5) {
+            var timestableOfDay = routes[userRoute].timesMonFri;
+            var nextFirstTime = routes[userRoute].timesMonFri[0][userDepartureIndex];
+            console.log(nextFirstTime);
+            sessionStorage.setItem("Next First Departure", nextFirstTime);
+        }
+        else if (dayOfWeek == 5) {
+            var timestableOfDay = routes[userRoute].timesMonFri.concat(routes[userRoute].timesFri);
+            var nextFirstTime = routes[userRoute].timesSat[0][userDepartureIndex];
+            console.log(nextFirstTime);
+            sessionStorage.setItem("Next First Departure", nextFirstTime);
+        }
+        else if (dayOfWeek == 6) {
+            var timestableOfDay = routes[userRoute].timesSat;
+            var nextFirstTime = routes[userRoute].timesSun[0][userDepartureIndex];
+            nextFirstTime = nextFirstTime.toFixed(2);
+            console.log(nextFirstTime);
+            sessionStorage.setItem("Next First Departure", nextFirstTime);
+        }
+        else if (dayOfWeek == 7) {
+            var timestableOfDay = routes[userRoute].timesSun;
+            var nextFirstTime = routes[userRoute].timesMonFri[0][userDepartureIndex];
+            console.log(nextFirstTime);
+            sessionStorage.setItem("Next First Departure", nextFirstTime);
+        }
+        //Looping through the timetable to get the times
+        for (var i = 0; i < timestableOfDay.length; i++) {
+            if (timeNow < timestableOfDay[i][userDepartureIndex]) {
+                var homeOnlyTime = timestableOfDay[timestableOfDay.length - 1];
+                sessionStorage.setItem("Home Only Time", homeOnlyTime);
+                //array[array.length - 1]
+                var nextDepartingTime = timestableOfDay[i][userDepartureIndex];
+                nextDepartingTime = nextDepartingTime.toFixed(2);
                 console.log (nextDepartingTime);
-                var nextArrivalTime = routes[userRoute].timesMonFri[i][userDepartureIndex + userArrivalIndex];
+                sessionStorage.setItem("Next Departure", nextDepartingTime);
+                var nextArrivalTime = timestableOfDay[i][userDepartureIndex + userArrivalIndex];
+                nextArrivalTime = nextArrivalTime.toFixed(2);
                 console.log (nextArrivalTime);
-                var followingDepartingTime = routes[userRoute].timesMonFri[i+1][userDepartureIndex];
+                sessionStorage.setItem("Next Arrival", nextArrivalTime);
+                var followingDepartingTime = timestableOfDay[i+1][userDepartureIndex];
+                followingDepartingTime = followingDepartingTime.toFixed(2);
+                sessionStorage.setItem("Following Departure", followingDepartingTime);
                 break;
             }
-
         }
+//        if (dayOfWeek > -1 && dayOfWeek < 5) {
+//            var nextFirstTime = routes[userRoute].timesMonFri[0][userDepartureIndex];
+//            console.log(nextFirstTime);
+//            for (var i = 0; i < routes[userRoute].timesMonFri.length; i++) {
+//                if (timeNow < routes[userRoute].timesMonFri[i][userDepartureIndex]) {
+//                    var nextDepartingTime = routes[userRoute].timesMonFri[i][userDepartureIndex];
+//                    nextDepartingTime = nextDepartingTime.toFixed(2);
+//                    console.log (nextDepartingTime);
+//                    var nextArrivalTime = routes[userRoute].timesMonFri[i][userDepartureIndex + userArrivalIndex];
+//                    nextArrivalTime = nextArrivalTime.toFixed(2);
+//                    console.log (nextArrivalTime);
+//                    var followingDepartingTime = routes[userRoute].timesMonFri[i+1][userDepartureIndex];
+//                    followingDepartingTime = followingDepartingTime.toFixed(2);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        else if (dayOfWeek == 5) {
+//            var nextFirstTime = routes[userRoute].timesSat[0][userDepartureIndex];
+//            console.log(nextFirstTime);
+//            var friMergedTimes = routes[userRoute].timesMonFri.concat(routes[userRoute].timesFri);
+//            console.log(friMergedTimes);
+//            for (var i = 0; i < friMergedTimes.length; i++) {
+//                if (timeNow < friMergedTimes[i][userDepartureIndex]) {
+//                    var nextDepartingTime = friMergedTimes[i][userDepartureIndex];
+//                    console.log (nextDepartingTime);
+//                    nextDepartingTime = nextDepartingTime.toFixed(2);
+//                    sessionStorage.setItem("Next Departure", nextDepartingTime);
+//                    var nextArrivalTime = friMergedTimes[i][userDepartureIndex + userArrivalIndex];
+//                    nextArrivalTime = nextArrivalTime.toFixed(2);
+//                    console.log (nextArrivalTime);
+//                    var followingDepartingTime = friMergedTimes[i+1][userDepartureIndex];
+//                    followingDepartingTime = followingDepartingTime.toFixed(2);
+//                    break;
+//                }
+//            }
+//        }
+//        else if (dayOfWeek == 6) {
+//            var nextFirstTime = routes[userRoute].timesSun[0][userDepartureIndex];
+//            console.log(nextFirstTime);
+//            for (var i = 0; i < routes[userRoute].timesSat.length; i++) {
+//                if (timeNow < routes[userRoute].timesSat[i][userDepartureIndex]) {
+//                    var nextDepartingTime = routes[userRoute].timesSat[i][userDepartureIndex];
+//                    nextDepartingTime = nextDepartingTime.toFixed(2);
+//                    console.log (nextDepartingTime);
+//                    var nextArrivalTime = routes[userRoute].timesSat[i][userDepartureIndex + userArrivalIndex];
+//                    nextArrivalTime = nextArrivalTime.toFixed(2);
+//                    console.log (nextArrivalTime);
+//                    var followingDepartingTime = routes[userRoute].timesSat[i+1][userDepartureIndex];
+//                    followingDepartingTime = followingDepartingTime.toFixed(2);
+//                    break;
+//                }
+//            }
+//        }
+//        else if (dayOfWeek == 7) {
+//            var nextFirstTime = routes[userRoute].timesMonFri[0][userDepartureIndex];
+//            console.log(nextFirstTime);
+//            for (var i = 0; i < routes[userRoute].timesSun.length; i++) {
+//                if (timeNow < routes[userRoute].timesSun[i][userDepartureIndex]) {
+//                    var nextDepartingTime = routes[userRoute].timesSun[i][userDepartureIndex];
+//                    nextDepartingTime = nextDepartingTime.toFixed(2);
+//                    console.log (nextDepartingTime);
+//                    var nextArrivalTime = routes[userRoute].timesSun[i][userDepartureIndex + userArrivalIndex];
+//                    nextArrivalTime = nextArrivalTime.toFixed(2);
+//                    console.log (nextArrivalTime);
+//                    var followingDepartingTime = routes[userRoute].timesSun[i+1][userDepartureIndex];
+//                    followingDepartingTime = followingDepartingTime.toFixed(2);
+//                    break;
+//                }
+//            }
+//        }
+//
+//
+//    var nextDeparture = sessionStorage.getItem("Next Departure");
+//    console.log(nextDeparture);
 
-        sessionStorage.setItem("Next Departure", nextDepartingTime);
-        sessionStorage.setItem("Next Arrival", nextArrivalTime);
-        sessionStorage.setItem("Following Departure", followingDepartingTime);
     }
 
-/******************************************************************************************
-                                    SEARCH RESULTS
-******************************************************************************************/
-function resultsPage() {
-
-    //DOM elements
-    var nextDepart = document.getElementById("depart");
-    var nextArrive = document.getElementById("arrive");
-    var departStop = document.getElementById("departStop");
-    var arriveStop = document.getElementById("arriveStop");
-    var chosenRoute = document.getElementById("chosenRoute");
-    var countdown = document.getElementById("countdown");
-    var followingDepart = document.getElementById("followingDepart");
-
-    nextDepart.innerHTML = sessionStorage.getItem("Next Departure");
-    nextArrive.innerHTML = sessionStorage.getItem("Next Arrival");
-    departStop.innerHTML = "&nbsp;" + sessionStorage.getItem("Departing Stop") + "&nbsp;";
-    arriveStop.innerHTML = "&nbsp;" + sessionStorage.getItem("Arrival Stop") + "&nbsp;";
-    chosenRoute.innerHTML = sessionStorage.getItem("User Route");
-    followingDepart.innerHTML = sessionStorage.getItem("Following Departure");
-
-    //Countdown
-    var timeNow = new Date().getHours() + "." + ("0" + new Date().getMinutes()).slice(-2); //Get time now in H.MM
-    console.log(timeNow);
-    var a = timeNow.split(".");
-    var timeNowMinutes = (+a[0]) * 60 + (+a[1]); //Convert H.MM to MM
-    console.log(timeNowMinutes);
-    var nextDeparture = sessionStorage.getItem("Next Departure");
-    var b = nextDeparture.split(".");
-    var nextDepartureMinutes = (+b[0]) * 60 + (+b[1]);
-    var duration = nextDepartureMinutes - timeNowMinutes; //Difference in MM between the two times
-    console.log(duration);
-
-    var hours = Math.floor(duration / 60);
-    var minutes = Math.floor(duration % 60);
-    console.log(hours);
-    console.log(minutes);
-
-    countdown.innerHTML = hours + "h" + "&nbsp;" + minutes + "m";
-}
-
-/* NEXT STEPS: Have usage of a class somewhere in the script. Start with making bus objects from a class, drawing info of each bus from the bus-stops-and-times-final.js
-
-Then make all reference to the class, rather than to the .js file. Basically, this script ----> Class -----> .js file
-
-        class Bus {
-            constructor(name, stops, timesMonFri, ) {
-                this.name = name;
-
-                this.routeSelection = document.getElementById("routes");
-            }
-
-        }
-
-*/
-
-
-//$(document).ready(function() {
-//
-//    $(".route").on('change', function() {
-//        userRoute = $(".route").text;
-//        console.log(userRoute);
-//    })
-//})
-
-/////////////////////////////////// Bus Slider on Home Page////////
+/////////////////////////////// Bus Slider on Home Page/////////////////
 document.querySelector("input[type=\"range\"]").onmouseup = function() {
     var theRange = this.value;
     if(theRange == 100) {
@@ -230,3 +288,74 @@ function unlock() {
     location.href = "search-result-page.html";
 }
 //////////////////////////////// Bus Slider on Home Page ////////////////
+
+
+
+
+
+/******************************************************************************************
+                                    SEARCH RESULTS
+******************************************************************************************/
+function resultsPage() {
+
+    //DOM elements
+    var nextDepart = document.getElementById("depart");
+    var nextArrive = document.getElementById("arrive");
+    var departStop = document.getElementById("departStop");
+    var arriveStop = document.getElementById("arriveStop");
+    var chosenRoute = document.getElementById("chosenRoute");
+    var countdown = document.getElementById("countdown");
+    var followingDepart = document.getElementById("followingDepart");
+    var resultBox = document.getElementById("result");
+    var afterHours = document.getElementById("after-hours");
+    var stop = document.getElementById("stop");
+    var route = document.getElementById("route");
+    var firstDepart = document.getElementById("firstTime");
+    var homeOnlyTime = document.getElementById("homeOnlyTime");
+
+    nextDepart.innerHTML = sessionStorage.getItem("Next Departure");
+    nextArrive.innerHTML = sessionStorage.getItem("Next Arrival");
+    departStop.innerHTML = "&nbsp;" + sessionStorage.getItem("Departing Stop") + "&nbsp;";
+    arriveStop.innerHTML = "&nbsp;" + sessionStorage.getItem("Arrival Stop") + "&nbsp;";
+    chosenRoute.innerHTML = sessionStorage.getItem("User Route");
+    followingDepart.innerHTML = sessionStorage.getItem("Following Departure");
+    firstDepart.innerHTML = sessionStorage.getItem("Next First Departure");
+    stop.innerHTML = sessionStorage.getItem("Departing Stop");
+    route.innerHTML = sessionStorage.getItem("User Route");
+    homeOnlyTime.innerHTML = sessionStorage.getItem("Home Only Time");
+
+    //Countdown
+    var timeNow = new Date().getHours() + "." + ("0" + new Date().getMinutes()).slice(-2); //Get time now in H.MM
+    console.log(timeNow);
+    var a = timeNow.split(".");
+    var timeNowMinutes = (+a[0]) * 60 + (+a[1]); //Convert H.MM to MM
+    console.log(timeNowMinutes);
+    var nextDeparture = sessionStorage.getItem("Next Departure");
+    console.log(nextDeparture);
+    var b = nextDeparture.split(".");
+    var nextDepartureMinutes = (+b[0]) * 60 + (+b[1]);
+    var duration = nextDepartureMinutes - timeNowMinutes; //Difference in MM between the two times
+    console.log(duration);
+
+    var hours = Math.floor(duration / 60);
+    var minutes = Math.floor(duration % 60);
+    console.log(hours);
+    console.log(minutes);
+
+    countdown.innerHTML = hours + "h" + "&nbsp;" + minutes + "m";
+
+    //After hours
+    var dayOfWeek = new Date().getDay();
+        if (dayOfWeek > -1 && dayOfWeek < 6) {
+            if (18.35 < timeNow && timeNow < 23.59) {
+                resultBox.style.display = "none";
+                afterHours.style.display = "block";
+            }
+        }
+        else if (dayOfWeek == 6 || dayOfWeek == 7) {
+            if (16.20 < timeNow && timeNow < 23.59) {
+                resultBox.style.display = "none";
+                afterHours.style.display = "block";
+            }
+        }
+}
